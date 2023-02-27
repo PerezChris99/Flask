c3371359511a2e9013d2399c25351ebd121defba
perezchris3599 <kweeziperez712@gmail.com>
2023-02-27 21:58:03 +0300

    templates

--- Flask Chat App/main.py
+++ Flask Chat App/main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcde'
 socketio = SocketIO(app)
 
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
 
