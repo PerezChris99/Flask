50652c41f5c2b13c890f48af9ad1f49b411fb999
perezchris3599 <kweeziperez712@gmail.com>
2023-02-28 13:57:37 +0300

    socket disconnect

--- Flask Chat App/main.py
+++ Flask Chat App/main.py
@@ -67,5 +67,20 @@ def connect(auth):
     rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
 
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left room {room}")
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
