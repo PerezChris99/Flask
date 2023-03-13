34db0d911dec0de9348717898a8ee8d9434896bb
perezchris3599 <kweeziperez712@gmail.com>
2023-03-13 15:06:59 +0300

    inheritence

--- FlaskBlog site official/app.py
+++ FlaskBlog site official/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 posts = [
     {
@@ -15,16 +15,16 @@ posts = [
     }
 ]
 
-
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
