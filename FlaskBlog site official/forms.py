7964ff7fe709818a542880d5697c77f30971230d
perezchris3599 <kweeziperez712@gmail.com>
2023-03-14 11:34:34 +0300

    reg form

--- FlaskBlog site official/forms.py
+++ FlaskBlog site official/forms.py
@@ -9,4 +9,4 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()]),
     password = PasswordField('Password', validators=[DataRequired()]),
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    
+    submit = SubmitField('Sign Up')
