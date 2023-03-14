cdbf3456d1063c42647f2f2d8a36e9c00bb2ba52
perezchris3599 <kweeziperez712@gmail.com>
2023-03-14 11:34:02 +0300

    registration form

---
+++ FlaskBlog site official/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
+
+class RegistrationForm(FlaskForm):
+    username = Stringfield('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()]),
+    password = PasswordField('Password', validators=[DataRequired()]),
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    
