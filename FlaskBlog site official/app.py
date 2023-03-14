1905da14dca000244b954a090f17b5f2ce954d18
perezchris3599 <kweeziperez712@gmail.com>
2023-03-14 11:38:52 +0300

    secret key

--- FlaskBlog site official/app.py
+++ FlaskBlog site official/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '123456'
+
+
 posts = [
     {
         'author': 'Corey Schafer',
--- FlaskBlog site official/forms.py
+++ FlaskBlog site official/forms.py
@@ -10,3 +10,9 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()]),
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()]),
+    password = PasswordField('Password', validators=[DataRequired()]),
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login Form')
